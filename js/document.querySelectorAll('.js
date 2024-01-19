fd87353c889bf4bf05dc3de55ade7b9a2ec1f0b6
henry1234-net <78443740+henry1234-net@users.noreply.github.com>
2024-01-19 00:03:43 +0000

    change

--- js/document.querySelectorAll('.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.image-grid img').forEach(image =>{
-    image.onclick = () =>{
-        document.querySelector('popup-image').style.display = 'block';
-        document.querySelector('popup-image img').src = image.getAttribute('src');
-    }
-});
